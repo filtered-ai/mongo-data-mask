6c65b490d64853f662d2d896e0cd660a197f1f99
John Ragone <ragone@usc.edu>
2021-07-26 11:32:52 -0400

    Initial commit

---
+++ fake.go
@@ -0,0 +1,17 @@
+package fake
+
+import "github.com/brianvoe/gofakeit/v6"
+
+gofakeit.Seed(0)
+
+gofakeit.Name()             // Markus Moen
+gofakeit.Email()            // alaynawuckert@kozey.biz
+gofakeit.Phone()            // (570)245-7485
+gofakeit.BS()               // front-end
+gofakeit.BeerName()         // Duvel
+gofakeit.Color()            // MediumOrchid
+gofakeit.Company()          // Moen, Pagac and Wuckert
+gofakeit.CreditCardNumber() // 4287271570245748
+gofakeit.HackerPhrase()     // Connecting the array won't do anything, we need to generate the haptic COM driver!
+gofakeit.JobTitle()         // Director
+gofakeit.CurrencyShort()    // USD
