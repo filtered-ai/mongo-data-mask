ebb7b259e2b1a3af9bf6be926ac046dedb5748e9
John Ragone <ragone@usc.edu>
2021-07-26 12:39:14 -0400

    Add .gitignore, basic mongo client usage

--- fake.go
+++ fake.go
@@ -1,17 +1,58 @@
-package fake
+package main
 
-import "github.com/brianvoe/gofakeit/v6"
+import (
+	"context"
+	"fmt"
+	"log"
+	"time"
 
-gofakeit.Seed(0)
+	"github.com/brianvoe/gofakeit/v6"
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
 
-gofakeit.Name()             // Markus Moen
-gofakeit.Email()            // alaynawuckert@kozey.biz
-gofakeit.Phone()            // (570)245-7485
-gofakeit.BS()               // front-end
-gofakeit.BeerName()         // Duvel
-gofakeit.Color()            // MediumOrchid
-gofakeit.Company()          // Moen, Pagac and Wuckert
-gofakeit.CreditCardNumber() // 4287271570245748
-gofakeit.HackerPhrase()     // Connecting the array won't do anything, we need to generate the haptic COM driver!
-gofakeit.JobTitle()         // Director
-gofakeit.CurrencyShort()    // USD
+func main() {
+	gofakeit.Seed(1)
+
+	// Connect to cluster
+	mongoURI := "mongodb://127.0.0.1:27017"
+	client, err := mongo.NewClient(options.Client().ApplyURI(mongoURI))
+	if err != nil {
+		log.Fatal(err)
+	}
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	err = client.Connect(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+	// Cancel context and disconnect last
+	defer cancel()
+	defer client.Disconnect(ctx)
+
+	// Get database
+	testDB := client.Database("testing")
+	// Drop old collections
+	collectionNames, _ := testDB.ListCollectionNames(ctx, bson.D{})
+	for _, collectionName := range collectionNames {
+		if err = testDB.Collection(collectionName).Drop(ctx); err != nil {
+			log.Fatal(err)
+		}
+	}
+
+	collection := "OrganizationCollection"
+	err = testDB.CreateCollection(ctx, collection)
+	if err != nil {
+		log.Fatal(err)
+	}
+	moreTestingCol := testDB.Collection(collection)
+
+	_, err = moreTestingCol.InsertOne(ctx, bson.D{
+		{Key: "title", Value: gofakeit.Name()},
+	})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Inserted one document into", collection+"!")
+}
---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/JRagone/fake
+
+go 1.16
+
+require (
+	github.com/brianvoe/gofakeit/v6 v6.5.0
+    go.mongodb.org/mongo-driver v1.7.0
+)
